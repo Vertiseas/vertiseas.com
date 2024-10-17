d7f4e3d59c5c7acd2dcc88dce04da2b2917b5487
Vertiseas <115049552+Vertiseas@users.noreply.github.com>
2024-10-16 21:03:28 -0400

    Removing garbo

--- scripts/pp.js
+++
@@ -1,4 +0,0 @@
-const playerNames = ["verti", "mier", "floo", "abri", "partack", "genki", "jelly", "jett", "yobu", "lance"];
-const playerCharacter = Object.fromEntries(playerNames.map(name => [name, { playerAvatar: `${name}.png` }]));
-
-console.log(playerCharacter.lance.playerAvatar);
